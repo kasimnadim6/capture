fb61283b7b796cf544f52c8e573b4ae04914beaa
Mahammad Kasim <kasimnadim6@gmail.com>
2021-02-21 11:18:06 +0530

    faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className='faq'>
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className='question'>
+        <h4>How Do I Start?</h4>
+        <div className='answer'>
+          <p>Lorem Ipsum  dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+            quisquam odio omnis voluptatum optio accusantium laboriosam maxime
+            odit velit, autem corporis aliquid fugiat quae eligendi corrupti
+            provident! Officiis, assumenda illum!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Daily Schedule</h4>
+        <div className='answer'>
+          <p>Lorem Ipsum  dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+            quisquam odio omnis voluptatum optio accusantium laboriosam maxime
+            odit velit, autem corporis aliquid fugiat quae eligendi corrupti
+            provident! Officiis, assumenda illum!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>Different payment methods</h4>
+        <div className='answer'>
+          <p>Lorem Ipsum  dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+            quisquam odio omnis voluptatum optio accusantium laboriosam maxime
+            odit velit, autem corporis aliquid fugiat quae eligendi corrupti
+            provident! Officiis, assumenda illum!
+          </p>
+        </div>
+      </div>
+      <div className='question'>
+        <h4>What products do you offer?</h4>
+        <div className='answer'>
+          <p>Lorem Ipsum  dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+            quisquam odio omnis voluptatum optio accusantium laboriosam maxime
+            odit velit, autem corporis aliquid fugiat quae eligendi corrupti
+            provident! Officiis, assumenda illum!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import SeriviceSection from "../components/SeriviceSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <SeriviceSection />
+      <FaqSection />
     </>
   );
 };
