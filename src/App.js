bd73f304328cccd78dd2087073bbc237ca99889e
Mahammad Kasim <kasimnadim6@gmail.com>
2021-02-21 10:36:37 +0530

    about section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import AboutUs from "./pages/AboutUs";
+
 function App() {
   return (
     <div className='App'>
-      <h1>Hey man</h1>
+      <AboutUs />
     </div>
   );
 }
